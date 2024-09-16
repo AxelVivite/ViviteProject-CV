09dfb216a57d9d38841cd329c55965e75175b8ef
Axel Virot <axel.virot@gmail.com>
2024-09-16 02:42:05 +0200

    [IMPR] Improved footer and thematicList reponsive and made copyright.config non jsx

--- src/components/organisms/thematicList.tsx
+++ src/components/organisms/thematicList.tsx
@@ -1,7 +1,7 @@
+import HeadingThree from "@atoms/typos/headingThree";
 import Subheading from "@atoms/typos/subheading";
 import Text from "@atoms/typos/text";
 import Thematic from "@interfaces/thematic";
-import HeadingThree from "@atoms/typos/headingThree";
 
 interface ThematicListProps {
   thematics: Thematic[];
@@ -9,24 +9,26 @@ interface ThematicListProps {
 
 const ThematicList: React.FC<ThematicListProps> = ({ thematics }) => {
   return (
-    <ul className="flex max-w-3xl flex-col gap-16 md:gap-20">
+    <ul className="flex max-w-3xl flex-col gap-20">
       {thematics.map((thematic) => (
         <li
           key={thematic.id}
-          className="flex flex-col gap-y-16 md:grid md:grid-cols-4 md:items-baseline md:border-l md:border-zinc-700/40 md:pl-6"
+          className="flex flex-col gap-y-8 md:grid md:grid-cols-4 md:items-baseline md:border-l md:border-zinc-700/40 md:pl-6"
         >
-          <Subheading className="text-sm tracking-normal">
+          <Subheading className="border-l-2 border-zinc-100 pl-4 text-sm tracking-normal md:border-l-0 md:pl-0">
             {thematic.title}
           </Subheading>
-          {thematic.articles.map((article) => (
-            <div
-              key={article.id}
-              className="relative flex flex-col gap-2 md:col-start-2 md:col-end-5"
-            >
-              <HeadingThree>{article.title}</HeadingThree>
-              <Text className="text-sm">{article.description}</Text>
-            </div>
-          ))}
+          <ul className="flex flex-col gap-y-16 md:col-start-2 md:col-end-5">
+            {thematic.articles.map((article) => (
+              <li
+                key={article.id}
+                className="relative flex flex-col gap-2"
+              >
+                <HeadingThree>{article.title}</HeadingThree>
+                <Text className="text-sm">{article.description}</Text>
+              </li>
+            ))}
+          </ul>
         </li>
       ))}
     </ul>
